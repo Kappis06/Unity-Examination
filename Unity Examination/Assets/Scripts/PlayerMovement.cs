142c13d2ea437740f95eeade8996dc0d7823ea63
Kasper <kasper.noren123@gmail.com>
2025-11-21 10:00:37 +0100

    Hopp

--- Unity Examination/Assets/Scripts/PlayerMovement.cs
+++ Unity Examination/Assets/Scripts/PlayerMovement.cs
@@ -5,14 +5,19 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private InputActionReference _actionMove;
+    [SerializeField] private InputActionReference _actionJump;
     
     private Rigidbody2D _rb;
     private SpriteRenderer _renderer;
     private Animator _animator;
 
+    [SerializeField] private Transform _groundCheck;
+    [SerializeField] private LayerMask _groundLayer;
+
     private float _inputMove;
 
     [SerializeField] private float _speed = 100f;
+    [SerializeField] private float _jumpForce = 100f;
     
     
     
@@ -27,6 +32,11 @@ public class PlayerMovement : MonoBehaviour
 
     private void Update()
     {
+        if (IsGrounded() && _actionJump.action.triggered)
+            _rb.AddForceY(_jumpForce, ForceMode2D.Impulse);
+        
+        
+        
         if (_inputMove > 0)
             _renderer.flipX = false;
         else if (_inputMove < 0)
@@ -41,5 +51,14 @@ public class PlayerMovement : MonoBehaviour
 
         
         _rb.linearVelocity = new Vector2(_inputMove * Time.fixedDeltaTime * _speed, _rb.linearVelocity.y);
+    }
+
+
+
+    private bool IsGrounded()
+    {
+        RaycastHit2D hitInfo = Physics2D.Raycast(_groundCheck.position, Vector2.down, 0.1f, _groundLayer);
+        
+        return hitInfo.collider != null;
     }
 }
