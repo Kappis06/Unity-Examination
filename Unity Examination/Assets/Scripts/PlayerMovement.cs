713a8585b04f1fac18ec7bff3635439ea412fdce
Kasper <kasper.noren123@gmail.com>
2025-11-21 09:21:13 +0100

    Animation

--- Unity Examination/Assets/Scripts/PlayerMovement.cs
+++ Unity Examination/Assets/Scripts/PlayerMovement.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -6,6 +7,8 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private InputActionReference _actionMove;
     
     private Rigidbody2D _rb;
+    private SpriteRenderer _renderer;
+    private Animator _animator;
 
     private float _inputMove;
 
@@ -16,10 +19,22 @@ public class PlayerMovement : MonoBehaviour
     private void Awake()
     {
         _rb = GetComponent<Rigidbody2D>();
+        _renderer = GetComponent<SpriteRenderer>();
+        _animator = GetComponent<Animator>();
     }
 
 
 
+    private void Update()
+    {
+        if (_inputMove > 0)
+            _renderer.flipX = false;
+        else if (_inputMove < 0)
+            _renderer.flipX = true;
+        
+        _animator.SetBool("IsWalking", _inputMove != 0);
+    }
+    
     private void FixedUpdate()
     {
         _inputMove = _actionMove.action.ReadValue<float>();
